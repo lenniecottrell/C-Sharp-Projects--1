a2622256d4b5b235c284af4a996f545e9e309c6e
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2020-08-27 17:01:20 -0700

    CarInsuranceApproval

---
+++ CarInsuranceApproval/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CarInsuranceApproval
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            string ageStr = Console.ReadLine();
+            int ageInt = Convert.ToInt32(ageStr);
+
+            Console.WriteLine("Have you ever had a DUI? true/false");
+            string DUIstatus = Console.ReadLine();
+            bool DUIbool = Convert.ToBoolean(DUIstatus);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string ticketsStr = Console.ReadLine();
+            int ticketsInt = Convert.ToInt32(ticketsStr);
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(ageInt > 15 && DUIbool == false && ticketsInt < 3);
+            Console.Read();
+
+
+        }
+    }
+}
