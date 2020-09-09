65b5b103311ae1b463b94ebf404e50c32f699f24
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2020-09-09 15:55:02 -0700

    Step 206

---
+++ Step206/Math.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step206
+{
+    public class Math
+    {
+        public static int addTen(int inputNum)
+        {
+            return inputNum + 10;
+        }
+        public static int timesTen(int inputNum)
+        {
+            return inputNum * 10;
+        }
+        public static int minusTen(int inputNum)
+        {
+            return inputNum - 10;
+        }
+    }
+}
---
+++ Step206/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step206
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to do some math: ");
+            int inputNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your number plus ten is " + Math.addTen(inputNum));
+            Console.WriteLine("Your number times ten is " + Math.timesTen(inputNum));
+            Console.WriteLine("Your number minus ten is " + Math.minusTen(inputNum));
+            Console.ReadLine();
+        }
+    }
+}
