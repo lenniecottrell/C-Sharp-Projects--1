6003edafd13979f24fa0dad9e323db5ee69c4986
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2020-09-12 12:12:12 -0700

    Step 220

---
+++ Step 220/DoSomeMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_220
+{
+    class DoSomeMath
+    {
+        public static int TwoNums(int num1, int num2 = 1)
+        {
+            return num1 * num2 + 10;
+        }
+    }
+}
---
+++ Step 220/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_220
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DoSomeMath.TwoNums(3, 5)); //instantiate the class
+            Console.WriteLine("Please enter a number: ");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another number, if you feel like it: ");
+            try
+            {
+                int input2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Your first number times your second number (if you felt like it) plus 10 equals " + DoSomeMath.TwoNums(input1, input2));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You didn't feel like putting in that second number, and ya know? That's okay!");
+                Console.WriteLine("Your first number plus 10 equals " + DoSomeMath.TwoNums(input1));
+            }
+            Console.ReadLine();
+        }
+    }
+}
