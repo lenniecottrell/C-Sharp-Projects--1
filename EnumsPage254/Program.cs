fe0b86e7169e741ab610cb8144d1ed1578ae8eed
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2020-09-16 17:38:36 -0700

    Page 254

---
+++ EnumsPage254/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsPage254
+{
+    class Program
+    {
+        enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main()
+        {
+            Console.WriteLine("Please enter the current day of the week: ");
+            string input = Console.ReadLine();
+            DaysOfTheWeek day;
+            try
+            {
+                day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), input);
+                Console.WriteLine("If you say so! Today is " + day);
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("WARNING!!! EXTREME FAILURE!! COMPUTER WILL SELF-DESTRUCT IN 10 SECONDS....");
+                Console.WriteLine("Please enter an actual day of the week (with proper capitalization please).");
+                Console.WriteLine(ex.Message);
+                day = DaysOfTheWeek.Monday;
+                Console.ReadLine();
+            }
+        }            
+
+    }
+}
--- TwentyOne/Deck.cs
+++ TwentyOne/Deck.cs
@@ -11,24 +11,17 @@ namespace TwentyOne
         public Deck() //this is a constructor. The name of it is always the same as the class
         {
             Cards = new List<Card>(); //this instantiates a new empty list using the property of the class
-            List<string> Suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" }; //list of suits
-            List<string> Faces = new List<string>() //list of faces
-            {
-                "Two", "Three", "Four", "Five", "Six", "Seven",
-                "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"
-            };
 
-            foreach (string face in Faces) // nested loop - for each face, loop through all the suits
+            for (int i = 0; i < 13; i++)
             {
-                foreach (string suit in Suits)
+                for (int j = 0; j < 4; j++)
                 {
-                    Card card = new Card(); // instantiate a new card
-                    card.Suit = suit; // assign suit to the one we're currently looping on
-                    card.Face = face; // assign face to the one we're currently looping on
-                    Cards.Add(card); // add the new card to the list of cards
+                    Card card = new Card();
+                    card.Face = (Face)i; //i is an integer, and we're casting i to the enum Face (outer for loop)
+                    card.Suit = (Suit)j; //j is an integer, and we're casting j to the enum Suit (inner for loop)
+                    Cards.Add(card); //add the new card to the cards list
                 }
             }
-
         }
         public List<Card> Cards { get; set; }
 
